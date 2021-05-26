23bc2276bf648f09eef99289443faba90d182a85
umashankar8897 <83775122+umashankar8897@users.noreply.github.com>
2021-05-26 21:29:47 +0530

    Add files via upload

---
+++ TASK3.py
@@ -0,0 +1,8 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+for number in list1:
+    if number >=0:
+        print(number)
+for positive in list2:
+    if positive >= 0:
+        print(positive)
